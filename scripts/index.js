c0cf15ac62cc875f2cb18616a7648a7a64e11373
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2022-02-04 22:32:07 +0500

    bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -25,17 +25,19 @@ popupEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByCkickOverlay);
 
+
 const formElement = document.querySelector('.popup__form-edit');
 const nameInput = formElement.querySelector('.popup__input-name');
 const jobInput = formElement.querySelector('.popup__input-description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
-  closePopup();
-};
 
+};
 formElement.addEventListener('submit', formSubmitHandler);
+
