56ca8c8be95aa76dd6b772275033ee7bd53c2748
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2022-02-04 23:05:28 +0500

    bugfix: БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popupElement = document.querySelector('.popup');
 const popupContainer = document.querySelector('.popup__container');
-const popupEditButton = document.querySelector('.button__edit-button');
-const popupCloseButton = popupContainer.querySelector('.button__close');
+const popupEditButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = popupContainer.querySelector('.profile__button-close');
 const popupSaveButton = popupContainer.querySelector('.button__save');
 const profileElement = document.querySelector('.profile');
 
